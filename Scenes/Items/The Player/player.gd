b6e1eb4c44711a07343447e5c9979c9b1701a8fe
Nayer Elbaiady <nelbaiady@gmail.com>
2026-04-01 04:42:42 +0200

    player is mortal now
    -while janky and a bit hardcoded, player and enemy can now damage each other
    -bounced rising no longer has variable height when jump button is pressed and released
    -player loses hp when damaged, and can die, which goes to edit mode

--- Scenes/Items/The Player/player.gd
+++ Scenes/Items/The Player/player.gd
@@ -1,5 +1,20 @@
 class_name Player extends CharacterBody2D
+#Node variables
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var mainCollision: CollisionPolygon2D = $PlayerCollision
+@onready var crouchCollision: CollisionPolygon2D = $PlayerCrouchCollision
+@onready var chourcCollision: CollisionPolygon2D = $PlayerChourcCollision
+@onready var uncrouchChecker: Area2D = $uncrouchChecker
+@onready var chourcChecker: Area2D = $chourcChecker
+@onready var stateMachine: StateMachine = $StateMachine
+@onready var playerProperties: PlayerProperties = $playerProperties
+
+#Exported stats
 @export var maxHealth : int = 3
+@export var invulnerabilityTime : float = 2 ##amount of time the player cannot take damage again for after taking damage
+@export var invulnerabilityTimer : float = invulnerabilityTime ##is 0 and increases over time when the player is invulnerable and when it is >= invulnerabilityTime, the player is no longer invulnerable 
+@export var invulnerabilityFlickerSpeed : float = 0.2 ##how quickly the flicker is toggled
 @export var gravity : int = 2400
 @export var terminalVelocity : int = 2000
 @export var topRunSpeed : int = 700
@@ -12,55 +27,64 @@ class_name Player extends CharacterBody2D
 @export var canCrouch := true
 @export var canChourc := false
 @export var canCrawl := true
-@export var gravityMult : float = 1
-@export var fallingGravityMult : float = 2
+@export var fallingGravityMult : float = 3
 @export var crouchInputThreshold : float = -0.8
 
-var currentHealth: int
+#Variables that change during gameplay
+var gravityMult : float = 1
+var currentHealth: int = maxHealth
 
 var jumping:bool = false
 var bounced:bool = false
+##make sure the player cannot bounce on multiple things in the same frame
+var bouncedThisFrame:bool = false
 
 var directionInput = Vector2.ZERO
 
-@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
-@onready var sprite: Sprite2D = $Sprite2D
-@onready var mainCollision: CollisionPolygon2D = $PlayerCollision
-@onready var crouchCollision: CollisionPolygon2D = $PlayerCrouchCollision
-@onready var chourcCollision: CollisionPolygon2D = $PlayerChourcCollision
-@onready var uncrouchChecker: Area2D = $uncrouchChecker
-@onready var chourcChecker: Area2D = $chourcChecker
-
-
-@onready var stateMachine: StateMachine = $StateMachine
-@onready var playerProperties: PlayerProperties = $playerProperties
-
+#Signals
 ##signal for when the player is bounced
 signal getBounced(velocity)
 
 func _ready() -> void:
 	globalEditor.player = self
 	getBounced.connect(bounce)
-	
 	signalBus.startEditMode.connect(enterEditState)
 	signalBus.startPlayMode.connect(enterPlayState)
 
 func bounce(bounceVelocity):
+	stateMachine._transitionToNextState("Rising",{"bounced":true,"bounceVelocity":bounceVelocity})
+	
 	velocity = bounceVelocity
 
 func enterEditState():
+	reset()
 	stateMachine._transitionToNextState("Editing")
 	sprite.flip_h = false
 func enterPlayState():
-	currentHealth = maxHealth
+	reset()
 	stateMachine._transitionToNextState("Idle")
 
+##resets stats
+func reset():
+	currentHealth = maxHealth
+	invulnerabilityTimer = invulnerabilityTime
+	gravityMult = 1
+	
 func _physics_process(_delta: float) -> void:
+	bouncedThisFrame = false
 	##Either mode
 	directionInput = (Input.get_vector("LstickL","LstickR","LstickD","LstickU") + Input.get_vector("dpadL","dpadR","dpadD","dpadU")).limit_length(1) 
 	if !globalEditor.isEditing:
 		if directionInput.x < 0: sprite.flip_h = true 
 		if directionInput.x > 0: sprite.flip_h = false
+		
+	#invulnerability animation
+	if invulnerabilityTimer < invulnerabilityTime:
+		invulnerabilityTimer += _delta
+		visible = fmod(invulnerabilityTimer,invulnerabilityFlickerSpeed*2)<invulnerabilityFlickerSpeed
+	else:
+		visible = true
+		invulnerabilityTimer = invulnerabilityTime
 
 ##plays an animation and also plays the reset track (reset no longer happens for now due to bugs)
 func resetPlay(animation:String):
@@ -81,7 +105,27 @@ func chourcCheck():
 	return true
 
 
-func _on_hitbox_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("hurtbox"):
-		getBounced.emit(velocity.slide(Vector2.UP.rotated(area.rotation)) + Vector2.UP.rotated(area.rotation) * (500))
-		
+##Dealing damage
+#func _on_hitbox_area_2d_area_entered(area: Area2D) -> void:
+	#if area.is_in_group("hurtbox") and !area.is_in_group("player"):
+		##if area.position.y > $hitboxArea2D/hitbox.position.y:
+
+##Taking damage
+func _on_hurtbox_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("hitbox") and !area.is_in_group("player"):
+		if invulnerabilityTimer >= invulnerabilityTime:
+			knockBack(area.global_position)
+			takeDamage()
+func takeDamage(damage:=1):
+	##make sure the player isnt invulnerable
+	currentHealth -= damage
+	if currentHealth <= 0:
+		die()
+	else:
+		invulnerabilityTimer = 0
+func knockBack(sourceLocation:Vector2, power=800):
+	##knock the player away from the source (plus a corrective y value to make sure the player isnt knocked into the air for no reason)
+	velocity = ((position-sourceLocation).normalized() + Vector2(0,0.5)) * power 
+func die():
+	print("bleh )X (you died)")
+	signalBus.startEditMode.emit()
--- Scenes/Items/The Player/states/rising.gd
+++ Scenes/Items/The Player/states/rising.gd
@@ -1,18 +1,26 @@
 #RISING STATE
 extends PlayerState
 
+var bounced := false
+
 func enter(_previous_state_path: String, _data := {}) -> void:
+	bounced = false
 	#player.animationPlayer.play(&"RESET")
 	#player.animationPlayer.advance(0)
 	#player.animationPlayer.play("jumpUp")
 	player.resetPlay("jumpUp")
-	player.velocity= Vector2(player.velocity.x, -player.jumpPower)
 	player.gravityMult = 1
+	
+	if _data.has("bounced"):
+		bounced = true
+		player.velocity = _data.bounceVelocity
+	else:
+		player.velocity= Vector2(player.velocity.x, -player.jumpPower)
 
 func physics_update(delta: float) -> void:
 	player.velocity.y += player.gravity * player.gravityMult * delta
 	player.velocity = Vector2(move_toward(player.velocity.x,player.directionInput.x * player.topRunSpeed,player.acceleration*delta),player.velocity.y)
-	if Input.is_action_just_released("jump") or player.velocity.y > 0:
+	if (Input.is_action_just_released("jump") and !bounced) or player.velocity.y > 0:
 		player.gravityMult = player.fallingGravityMult
 		
 	player.move_and_slide()
